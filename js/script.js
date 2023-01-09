5f2e58365156962b6200071600dee3c8479eb5e0
zewdu erkyhun <zewdu444@gmail.com>
2023-01-09 15:33:40 +0300

    Setup javascript file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 1. create abook collection array of objects
+// 2. create constructor for author and title.
+// 3. create afunction to add new book to the collection
+// 4 .Creat afuction to remove book from acollection
+// 5.Display all saved books in the collection
+// 6. Data should be store in localstorage
